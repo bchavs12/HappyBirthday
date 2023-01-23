1a8ff5d3237eccb6237bd1196958fa6cf32a1213
bchavs12 <chavesbruno699@gmail.com>
2023-01-23 20:03:17 -0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import newPage from "./components/NewPage.vue";
+</script>
+
+<template>
+  <newPage />
+</template>
+
+<style>
+#app {
+  font-family: "Merriweather Sans", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: auto;
+}
+</style>
---
+++ src/components/NewPage.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import JSConfetti from "js-confetti";
+
+let name: string | object = ref(prompt("Qual seu Nome?"));
+
+const confetti = new JSConfetti();
+
+function showConfetti() {
+  confetti.addConfetti();
+}
+
+showConfetti();
+</script>
+
+<template>
+  <h1 @click="showConfetti">🎉 Happy Birthday, {{ name }}!</h1>
+  <button @click="showConfetti">CONFETTI</button>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
+button {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  color: wheat;
+  background: blueviolet;
+  font-weight: bold;
+}
+</style>
